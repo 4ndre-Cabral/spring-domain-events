62b880f7e9bedd0bff5a80c7ee47d7cf891fe756
a.cabral <a.cabral@reply.com>
2022-07-25 15:08:32 -0300

    initial commit

---
+++ src/main/java/com/domain/events/springdomainevents/SpringDomainEventsApplication.java
@@ -0,0 +1,13 @@
+package com.domain.events.springdomainevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDomainEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDomainEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domain/events/springdomainevents/SpringDomainEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.events.springdomainevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDomainEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
